776ead13961fbcb6d10909681f68526769f4266b
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2022-02-05 15:37:43 +0900

    modyfy the design

---
+++ test14/src/application/MenuController.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    @FXML
+    void onClickSearch(ActionEvent event) throws IOException {     	
+		Parent root = FXMLLoader.load(getClass().getResource("NewWindow.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Student Informations");
+		stage.show();
+    	
+    }
+
+    @FXML
+    void onClickTable1(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("View1.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Student Informations");
+		stage.show();
+    }
+    @FXML
+    void onClickTable2(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("View2.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Student Informations");
+		stage.show();
+    }
+    @FXML
+    void onClickBacktoLogin(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setTitle("Login Form");
+		stage.setScene(scene);
+		stage.show();
+    }
+
+}
